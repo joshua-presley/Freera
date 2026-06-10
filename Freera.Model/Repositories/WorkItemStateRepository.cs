a88adff3537422c789ae89528af422836120d8ba
joshua-presley <joshua.m.presley@icloud.com>
2026-06-10 14:48:45 -0400

    Add repos for WorkItem and WorkItemState

---
+++ Freera.Model/Repositories/WorkItemStateRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freera.Model.Repositories
+{
+    public class WorkItemStateRepository: Repository<WorkItemState>
+    {
+    }
+}
