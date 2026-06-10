727b5466a2e8aebcbd315202689b68821ddab4ef
joshua-presley <joshua.m.presley@icloud.com>
2026-06-10 14:25:14 -0400

    Add the base repository implementation

---
+++ Freera.Interfaces/Repositories/Base/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freera.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> QueryAll();
+        T QueryById(Guid id);
+        void Add(T item);
+        void Update(T item);
+        void Delete(Guid id);
+        void Delete(T item);
+    }
+}
